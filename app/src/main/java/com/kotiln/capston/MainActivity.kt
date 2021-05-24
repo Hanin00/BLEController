cea9f590cd405ec8c545663e685f90b7d35a9473
haeun <hauen5419@gmail.com>
2021-05-25 03:23:46 +0900

    ble 연결 20%

--- app/src/main/java/com/kotiln/capston/MainActivity.kt
+++ app/src/main/java/com/kotiln/capston/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.kotiln.capston
 
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import android.widget.ToggleButton
 import androidx.viewbinding.ViewBinding
 import com.kotiln.capston.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,12 +14,32 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val REQUEST_ENABLE_BT=1
+    private var bluetoothAdapter: BluetoothAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+
+        if(bluetoothAdapter!=null){
+            // Device doesn't support Bluetooth
+            if(bluetoothAdapter?.isEnabled==false){
+                //어댑터 연결 불가능할 때,
+                binding.btnMainConnect.setText("연결 해제 하기")
+            } else{
+                binding.btnMainConnect.setText("연결 하기")
+            }
+        }
+
+        binding.btnMainConnect.setOnClickListener {
+            bluetoothOnOff()
+        }
+
 
 
         btn_main_go.setOnClickListener {
@@ -43,4 +66,20 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+    fun bluetoothOnOff(){
+        if (bluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+            Log.d("bluetoothAdapter","Device doesn't support Bluetooth")
+        }else{
+            if (bluetoothAdapter?.isEnabled == false) { // 블루투스 꺼져 있으면 블루투스 활성화
+                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+                binding.btnMainConnect.setText("연결 해제 하기")
+            } else{ // 블루투스 켜져있으면 블루투스 비활성화
+                bluetoothAdapter?.disable()
+                binding.btnMainConnect.setText("연결 하기")
+            }
+        }
+    }
+
 }
