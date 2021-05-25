a96602160ba60f8fdcc9f3f0ceb4c0e4ccd7cd4d
haeun <hauen5419@gmail.com>
2021-05-25 20:23:07 +0900

    android 문서 기준으로 변경 직전

--- app/src/main/java/com/kotiln/capston/MainActivity.kt
+++ app/src/main/java/com/kotiln/capston/MainActivity.kt
@@ -2,6 +2,7 @@ package com.kotiln.capston
 
 import android.bluetooth.BluetoothAdapter
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -16,6 +17,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val REQUEST_ENABLE_BT=1
     private var bluetoothAdapter: BluetoothAdapter? = null
+
+    //ble 지원 기기에만 앱 제공
+    private fun PackageManager.missingSystemFeature(name: String): Boolean = !hasSystemFeature(name)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +42,11 @@ class MainActivity : AppCompatActivity() {
 
         binding.btnMainConnect.setOnClickListener {
             bluetoothOnOff()
+        }
+
+        packageManager.takeIf { it.missingSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE) }?.also {
+            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show()
+            finish()
         }
 
 
